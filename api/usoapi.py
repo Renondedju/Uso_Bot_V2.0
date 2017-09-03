b740b38e3da1b3fe72affc92a561e608b1e4d862
James Guess <jamesguessis@gmail.com>
2017-09-03 09:14:40 -0400

    parameter parsing looks nicer

--- api/usoapi.py
+++ api/usoapi.py
@@ -1,6 +1,6 @@
-#!flask/bin/python
 from flask import Flask, jsonify
 from datetime import datetime, timedelta
+from urllib.parse import parse_qs
 import sqlite3
 
 # The rate tracker, tracks rate per key
@@ -15,22 +15,13 @@ limits = {}
 app = Flask(__name__, static_url_path = "")
 
 def parse_params(parameters):
-    """Parse the parameters passed to the api"""
-    params = parameters.split('&')
-    # Set the defaults for the optional parameters
-    ranked, mods, pp = 0, '', 0 # Note: ranked and pp are set to 0 like a 0 and 1 on off switch
-    key, username = None, None # This allows us to throw a bad request if these aren't defined
-    for param in params:
-        if param.startswith('k='):
-            key = param.replace('k=', '')
-        elif param.startswith('u='):
-            username = param.replace('u=', '')
-        elif param.startswith('m='):
-            mods = param.replace('m=', '')
-        elif param.startswith('p='):
-            pp = (0, int(param.replace('p=', '')))[param.replace('p=', '').replace('.', '', 1).isdigit()]
-        elif param.startswith('r='):
-            ranked = (False, True)[param.replace('r=', '') == '1']
+    """Process the parsed the parameters passed to the api"""
+    params = parse_qs(parameters)
+    key = params['k'][0] if 'k' in params else None
+    username = params['u'][0] if 'u' in params else None
+    mods = params['m'][0] if 'm' in params else ''
+    pp = float(params['p'][0]) if 'p' in params and params['p'][0].replace('.', '', 1).isdigit() else 0
+    ranked = not ('r' in params) or ('r' in params and params['r'][0] == '1')
     return (key, username, mods, pp, ranked)
 
 def check_key(apikey):
