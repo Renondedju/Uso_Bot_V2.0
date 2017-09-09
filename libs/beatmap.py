3899276d6b49b0b018985fbc5e1fe5f1f88c5d4f
Renondedju <lefantomedelaredstone@gmail.com>
2017-09-09 14:32:59 +0200

    Added the start of the Beatmp class

--- libs/beatmap.py
+++ libs/beatmap.py
@@ -1,5 +1,78 @@
 # -*- coding: utf-8 -*-
 
+import sqlite3
+
+class Beatmap():
+
+	def __init__(self, beatmap_id, database_path):
+
+		self.beatmap_id = beatmap_id
+		self.uso_id = None
+		self.beatmapset_id = None
+
+		self.bpm = None
+		self.difficultyrating = 0
+		self.aim_stars = 0
+		self.speed_stars = 0
+
+		self.playstyle = self.speed_stars/self.difficultyrating # 0 = aim (jumps) , 1 = speed (stream)
+
+		self.diff_size = None
+		self.diff_overall = None
+		self.diff_approach = None
+		self.diff_drain = None
+		self.hit_length = None
+		self.total_length = None
+		self.max_combo = None
+
+		self.artist = None
+		self.creator = None
+		self.title = None
+		self.version = None
+		self.mode = None
+		self.tags = None
+		self.approved = None
+		self.approved_date = None
+		self.last_update = None
+
+		self.PP_100 = None
+		self.PP_100_HR = None
+		self.PP_100_HD = None
+		self.PP_100_DT = None
+		self.PP_100_DTHD = None
+		self.PP_100_DTHR = None
+		self.PP_100_HRHD = None
+		self.PP_100_DTHRHD = None
+
+		self.PP_99 = None
+		self.PP_99_HR = None
+		self.PP_99_HD = None
+		self.PP_99_DT = None
+		self.PP_99_DTHD = None
+		self.PP_99_DTHR = None
+		self.PP_99_HRHD = None
+		self.PP_99_DTHRHD = None
+
+		self.PP_98 = None
+		self.PP_98_HR = None
+		self.PP_98_HD = None
+		self.PP_98_DT = None
+		self.PP_98_DTHD = None
+		self.PP_98_DTHR = None
+		self.PP_98_HRHD = None
+		self.PP_98_DTHRHD = None
+
+		self.PP_97 = None
+		self.PP_97_HR = None
+		self.PP_97_HD = None
+		self.PP_97_DT = None
+		self.PP_97_DTHD = None
+		self.PP_97_DTHR = None
+		self.PP_97_HRHD = None
+		self.PP_97_DTHRHD = None
+
+	
+
 """
 uso_id
 beatmap_id
