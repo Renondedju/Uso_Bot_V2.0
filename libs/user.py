f2ad9a0bd195b207067ec51785ec989f0c1d63b8
Renondedju <lefantomedelaredstone@gmail.com>
2017-10-23 19:16:53 +0200

    User import fully done !

--- libs/user.py
+++ libs/user.py
@@ -6,7 +6,7 @@ import json
 
 from osuapi  import get_user, get_user_best
 from beatmap import Beatmap
-from mods    import Mode
+from mods    import Mode, Mods
 
 class User():
     """ User informations """
@@ -21,58 +21,58 @@ class User():
         self.discord_icon = None
 
         #Global informations
-        self.osu_id = osu_id
-        self.uso_id = None
+        self.osu_id     = osu_id
+        self.uso_id     = None
         self.discord_id = None
-        self.osu_name = None
-        self.rank = None
+        self.osu_name   = None
+        self.rank       = None
 
         #User performances
-        self.accuracy_average = None
-        self.pp_average = None
-        self.bpm_low = None
-        self.bpm_average = None
-        self.bpm_high = None
-        self.od_average = None
-        self.ar_average = None
-        self.cs_average = None
-        self.len_average = None #Drain
+        self.accuracy_average   = None
+        self.pp_average         = None
+        self.bpm_low            = None
+        self.bpm_average        = None
+        self.bpm_high           = None
+        self.od_average         = None
+        self.ar_average         = None
+        self.cs_average         = None
+        self.len_average        = None #Drain
 
         #Mods playrate
-        self.Nomod_playrate = None
-        self.HR_playrate = None
-        self.HD_playrate = None
-        self.DT_playrate = None
-        self.DTHD_playrate = None
-        self.DTHR_playrate = None
-        self.HRHD_playrate = None
-        self.DTHRHD_playrate = None
+        self.Nomod_playrate     = None
+        self.HR_playrate        = None
+        self.HD_playrate        = None
+        self.DT_playrate        = None
+        self.DTHD_playrate      = None
+        self.DTHR_playrate      = None
+        self.HRHD_playrate      = None
+        self.DTHRHD_playrate    = None
 
         #Mods recommended
-        self.Nomod_recommended = None
-        self.HR_recommended = None
-        self.HD_recommended = None
-        self.DT_recommended = None
-        self.DTHD_recommended = None
-        self.DTHR_recommended = None
-        self.HRHD_recommended = None
+        self.Nomod_recommended  = None
+        self.HR_recommended     = None
+        self.HD_recommended     = None
+        self.DT_recommended     = None
+        self.DTHD_recommended   = None
+        self.DTHR_recommended   = None
+        self.HRHD_recommended   = None
         self.DTHRHD_recommended = None
 
         #Playstyle -> Jumps 0 -----|----- 1 Stream
         self.playstyle = None
 
         #Api settings
-        self.api_key = None
+        self.api_key      = None
         self.request_rate = 0 # Requests/min (beatmaps requests)
 
         #Money bonuses
         self.requests_max = 5
-        self.donations = 0
+        self.donations    = 0
 
         #Patch
         self.last_discord_patch_used = "0.0.0"
-        self.last_irc_patch_used = "0.0.0"
-        self.last_time_played = 0 #timestamp
+        self.last_irc_patch_used     = "0.0.0"
+        self.last_time_played        = 0 #timestamp
 
         self.load_user_profile()
 
@@ -251,12 +251,15 @@ class User():
         return
 
     def update_user_stats(self):
+        """ Updating user stats """
 
         if not self.osu_id:
             return
 
         userinfo = get_user(self.settings['osu_api_key'], self.osu_id, Mode.Osu)[0]
         userbest = get_user_best(self.settings['osu_api_key'], self.osu_id, Mode.Osu, 20)
+
+        # print (userbest)
 
         self.osu_name           = userinfo['username']
         self.rank               = int(userinfo['pp_rank'])
@@ -283,18 +286,37 @@ class User():
             self.ar_average         += btmap.ar
             self.od_average         += btmap.od
 
-            self.bpm_average.extend([timing.ms_per_beat for timing in btmap.timing_points if timing.change])
-            self.playstyle += stars.speed / stars.total
+            self.bpm_average.append(beatmap.get_bpm(btmap))
+            self.playstyle   += stars.speed / stars.total
+
+            #Mods playrate
+
+            if (score['enabled_mods'] == '0'):   #None
+                self.Nomod_playrate  += 100.0 / 20.0;
+            if (score['enabled_mods'] == '16'):  #HR
+                self.HR_playrate     += 100.0 / 20.0;
+            if (score['enabled_mods'] == '8'):   #HD
+                self.HD_playrate     += 100.0 / 20.0;
+            if (score['enabled_mods'] == '64'):  #DT
+                self.DT_playrate     += 100.0 / 20.0;
+            if (score['enabled_mods'] == '72'):  #DTHD
+                self.DTHD_playrate   += 100.0 / 20.0;
+            if (score['enabled_mods'] == '80'):  #DTHR
+                self.DTHR_playrate   += 100.0 / 20.0;
+            if (score['enabled_mods'] == '24'):  #HRHD
+                self.HRHD_playrate   += 100.0 / 20.0;
+            if (score['enabled_mods'] == '88'):  #DTHDHR
+                self.DTHRHD_playrate += 100.0 / 20.0;
 
         self.playstyle          /= 20.0
-        self.pp_average         /= 20.0
-        self.cs_average         /= 20.0
-        self.ar_average         /= 20.0
-        self.od_average         /= 20.0
-        self.accuracy_average   /= 0.2
-        self.bpm_high           = max(self.bpm_average)
-        self.bpm_low            = min(self.bpm_average)
-        self.bpm_average        = (sum(self.bpm_average) / len(self.bpm_average))
+        self.pp_average         = round(self.pp_average / 20.0)
+        self.cs_average         = round(self.cs_average / 20.0)
+        self.ar_average         = round(self.ar_average / 20.0)
+        self.od_average         = round(self.od_average / 20.0)
+        self.accuracy_average   = round(self.accuracy_average / 0.2)
+        self.bpm_high           = round(max(self.bpm_average))
+        self.bpm_low            = round(min(self.bpm_average))
+        self.bpm_average        = round(sum(self.bpm_average) / len(self.bpm_average))
     
         return
 
@@ -332,7 +354,7 @@ class User():
         print("|")
         print("---- Mods playrate ----")
         print("|")
-        print("|-Nomod  = {}%".format(self.Nomod_playrate))
+        print("|-Nomod      = {}%".format(self.Nomod_playrate))
         print("|-HR         = {}%".format(self.HR_playrate))
         print("|-HD         = {}%".format(self.HD_playrate))
         print("|-DT         = {}%".format(self.DT_playrate))
@@ -353,7 +375,7 @@ class User():
         print("---- Money bonuses ----")
         print("|")
         print("|-requests_max   = {} beatmaps/request".format(self.requests_max))
-        print("|-donations  = {}€".format(self.donations))
+        print("|-donations      = {}€".format(self.donations))
         print("|")
         print("---- Patch ----")
         print("|")
