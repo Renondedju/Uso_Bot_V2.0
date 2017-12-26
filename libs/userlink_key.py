3803e519bf18091267d0ef8bd17d1ee2c47328a4
Renondedju <lefantomedelaredstone@gmail.com>
2017-12-26 21:24:12 +0100

    Fixed link user lib

--- libs/userlink_key.py
+++ libs/userlink_key.py
@@ -2,20 +2,18 @@ import time
 import string
 import random
 import memcache
-import update_stats
-import sqlite3
-from osuapi import OsuApi, ReqConnector
-import constants
+
+from libs.user import User
 
 class key(object):
-
+	""" Secret key object """
 	def __init__(self, size = 6, validity = 600):
 		self.__key = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
 		self.__validity = validity
 		self.__creation_time = time.time()
 
 	def get(self):
-
+		""" Get method """
 		if time.time() - self.__creation_time <= self.__validity:
 			return self.__key
 		else:
@@ -23,6 +21,7 @@ class key(object):
 			return None
 
 	def invalidate(self):
+		""" Invalidating the key """
 		self.__key = None
 
 class userlink:
@@ -31,12 +30,9 @@ class userlink:
 		self.shared = memcache.Client(['127.0.0.1:11211'], debug=0)
 		self.keys = {} # Osu id : [Key, Discord Id]
 		self.shared.set('keys', self.keys)
-		self.database_connexion = database_connexion
-		self.database_cursor = database_connexion.cursor()
-		self.api = OsuApi(constants.Api.osuApiKey, connector=ReqConnector())
 
 	def generate_new_key(self, osu_id, discord_id):
-
+		""" Generating a new key """
 		discord_id = int(discord_id)
 
 		self.keys = self.shared.get('keys')
@@ -45,24 +41,22 @@ class userlink:
 
 		return self.keys[osu_id][0].get()
 
-	def link_account(self, osu_id, key, osu_name):
+	def link_account(self, osu_id: int, key: string, osu_name: string):
+		""" Trys to link an account """
 
-		osu_id = int(osu_id)
-
+		#Updating the keys cache
 		self.keys = self.shared.get('keys')
-		self.database_cursor.execute("SELECT * FROM users WHERE osuId = ?", (osu_id,))
-
-		if not self.database_cursor.fetchall():
-			self.database_cursor.execute("INSERT INTO users (osuId, osuName) VALUES (?)", (osu_id, osu_name,))
-			self.database_connexion.commit()
+		#Creating the user
+		user = User(osu_id)
 
 		if self.keys[osu_id][0].get() == key:
-			self.database_cursor.execute("UPDATE users SET discordId = ?, rank = ? WHERE osuId = ?", (self.keys[osu_id][1], "USER", osu_id,))
-			self.database_connexion.commit()
-			update_stats.update_stats(self.keys[osu_id][1], self.database_connexion, self.api)
+			# The key is right : linking user
+			user.discord_id = self.keys[osu_id][1]
+			user.save_user_profile()
 			return
 			
 		elif self.keys[osu_id][0].get() == None:
+			# This key seems to be expired
 			self.keys.pop(osu_id)
 			self.shared.set('keys', self.keys)
 			raise ValueError("The key you are looking for expired")
