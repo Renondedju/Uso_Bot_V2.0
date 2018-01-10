87217938c39e8de39ea60997b72151f807d82e2d
James Guess <jamesguessis@gmail.com>
2018-01-10 15:00:51 -0500

    A cleaner irc bot class?

---
+++ irc/irc.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import re, os, sys
+import socket
+
+class IRCbot:
+    def __init__(self, server, nick):
+        self.server = server
+        self.nick = nick
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send(self, target, msg):
+        self.irc.send('PRIVMSG {target} {msg}\n'.encode())
+
+    def connect(self, password):
+        print(f'connecting to: {self.server}')
+        self.irc.connect((self.server, 6667))
+        print('connected!')
+        self.irc.send(f'USER {self.nick} {self.nick} {self.nick} :Test python irc bot script\n'.encode())
+        self.irc.send(f'PASS {password}\n'.encode())
+        self.irc.send(f'NICK {self.nick}\n'.encode())
+        self.irc.send('WHOIS tillerino\n'.encode())
+
+    def get_text(self):
+        text = self.irc.recv(2048).decode('utf-8')
+        if text.find('PING') != -1:
+            print('My heart is fluttering')
+            self.irc.send(f'PONG {text.split()[1]}\n'.encode()) 
+        return text
+
+if __name__ == '__main__':
+    irc = IRCbot('irc.ppy.sh', 'username')
+    irc.connect('password')
+    online = True
+    while online == True:
+        text = irc.get_text()
+        if text.strip() != '':
+            print(text)
