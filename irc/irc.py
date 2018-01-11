05ce9fc3d68dfa0d52d2a81bcb7212354925db64
James Guess <jamesguessis@gmail.com>
2018-01-11 15:09:57 -0500

    Use regex for irc line parsing

--- irc/irc.py
+++ irc/irc.py
@@ -11,6 +11,10 @@ class IRCbot:
     def send(self, target, msg):
         self.irc.send('PRIVMSG {target} {msg}\n'.encode())
 
+    def pong(self, msg):
+        self.irc.send(f'PONG {msg[-1]}\n'.encode())
+        #print('My heart fluttered.')
+
     def connect(self, password):
         print(f'connecting to: {self.server}')
         self.irc.connect((self.server, 6667))
@@ -20,17 +24,27 @@ class IRCbot:
         self.irc.send(f'NICK {self.nick}\n'.encode())
 
     def get_text(self):
+        messages = []
         text = self.irc.recv(2048).decode('utf-8')
-        if text.find('PING') != -1:
-            print('My heart is fluttering')
-            self.irc.send(f'PONG {text.split()[1]}\n'.encode()) 
-        return text
+        for line in text.split('\n'):
+            if line.strip() != '':
+                parsed = self.parse_line(line)
+                if parsed[1] == 'PING': self.pong(parsed)
+                else: messages.append(parsed)
+        return messages
+
+    def parse_line(self, line):
+        parsed = re.findall('^(?:[:](\S+) )?(\S+)(?: (?!:)(.+?))?(?: [:](.+))?$', line)
+        return parsed[0]
+
+    def close(self):
+        self.irc.shutdown()
+        self.irc.close()
 
 if __name__ == '__main__':
     irc = IRCbot('irc.ppy.sh', 'username')
     irc.connect('password')
     while True:
-        text = irc.get_text()
-        for line in text.split('\n'):
-            if line.strip() != '':
-                print(line)
+        messages = irc.get_text()
+        for msg in messages:
+            print(msg)
