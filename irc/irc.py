7eb15167cf1894e1f8e686b176adca3250eb8bb6
James Guess <jamesguessis@gmail.com>
2018-01-12 00:12:22 -0500

    Fixed sending messages
    And also print that we're sending them

--- irc/irc.py
+++ irc/irc.py
@@ -13,7 +13,8 @@ class IRCbot:
 
     def send(self, target, msg):
         '''Sends a private message to the target'''
-        self.irc.send('PRIVMSG {target} {msg}\n'.encode())
+        self.irc.send(f'PRIVMSG {target} {msg}\n'.encode())
+        print(f'To {target}: {msg}')
 
     def pong(self, msg):
         '''Responds to pings sent by the IRC server'''
@@ -23,12 +24,11 @@ class IRCbot:
 
     def connect(self):
         '''Connects and logs into the IRC server'''
-        print(f'connecting to: {self.server}')
         self.irc.connect((self.server, 6667))
-        print('connected!')
+        print(f'Connected to: {self.server}')
         self.irc.send(f'USER {self.nick} {self.nick} {self.nick} :Test python irc bot script\n'.encode())
-        # Logging in
         self.irc.send(f'PASS {self.passw}\n'.encode())
+        self.irc.send(f'NICK {self.nick}\n'.encode())
 
     def get_text(self):
         '''Recieve, split, and parse data coming from the irc server'''
@@ -57,8 +57,9 @@ class IRCbot:
 
     def reconnect(self):
         '''I'm hoping this makes reconnecting easy'''
-        print('possible disconnected')
+        print('Reconnecting...')
         self.irc.close()
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect()
 
 if __name__ == '__main__':
@@ -66,11 +67,20 @@ if __name__ == '__main__':
     irc = IRCbot('irc.ppy.sh', 'username', 'password')
     irc.connect()
     stayonline = True
-    try: # Most of this is to test shutting the bot down
+    try:
         while stayonline == True:
             for msg in irc.get_text():
-                if msg[-1] == '!quit w xd':
-                    print('I was told to quit w\nSo I will, bye.')
+                if msg[1] == 'PRIVMSG':
+                    sender = msg[0].split('!')[0]
+                    print(f'From {sender}: {msg[3]}')
+                    if msg[3] == 'test ping':
+                        irc.send(sender, 'pong my dude')
+                elif msg[1] in ['001', '372', '375', '376']:
+                    print(msg[3].strip())
+                elif msg[1] in ['311', '319', '312', '318', '401']:
+                    print(f'Whois {msg[2].split(" ")[1]}: {msg[3].strip()}')
+                elif msg[1] == '464':
+                    print('Bad authentication token.')
                     stayonline = False
                     break
                 else: print(msg)
@@ -102,10 +112,20 @@ if __name__ == '__main__':
 #     ('cho.ppy.sh', '372', 'Jamu', '- status: https://twitter.com/osustatus\r')
 #     ('cho.ppy.sh', '372', 'Jamu', '- boat:   https://twitter.com/banchoboat\r')
 #     ('cho.ppy.sh', '376', 'Jamu', '-\r')
+# This is what you get when you give a bad pass
+# MOTD seems to switch to telling you it's a bad password
+# Safe to assume if we see 464, we need to stop trying to reconnect.
+#     ('cho.ppy.sh', '372', 'jamu', 'Welcome to osu!bancho.\r')
+#     ('cho.ppy.sh', '372', 'jamu', '-\r')
+#     ('cho.ppy.sh', '372', 'jamu', '- You are required to authenticate before accessing this service.\r')
+#     ('cho.ppy.sh', '372', 'jamu', '- Please click the following link to receive your password:\r')
+#     ('cho.ppy.sh', '372', 'jamu', '- https://osu.ppy.sh/p/irc\r')
+#     ('cho.ppy.sh', '372', 'jamu', '-\r')
+#     ('cho.ppy.sh', '464', 'jamu', 'Bad authentication token.\r')
 # After repetetive tests 311 seems to be user definitions? 319 seems to be channels the user is in
 # 312 seems to be hostnames or something, possibly used to indicate if online in game?
 # 318 ends WHOS, obviously, 401 seems to be that the user isn't online, possible need to strip?
-#     ('cho.ppy.sh', '311', 'Jamu Tillerino https://osu.ppy.sh/u/2070907 *', 'https://osu.ppy.sh/u/2070907\r                                                                                                                                                                         ')
+#     ('cho.ppy.sh', '311', 'Jamu Tillerino https://osu.ppy.sh/u/2070907 *', 'https://osu.ppy.sh/u/2070907\r')
 #     ('cho.ppy.sh', '319', 'Jamu Tillerino', '#osu \r')
 #     ('cho.ppy.sh', '312', 'Jamu Tillerino cho.ppy.sh', 'osu!Bancho\r')
 #     ('cho.ppy.sh', '318', 'Jamu Tillerino', 'End of /WHOIS list.\r')
