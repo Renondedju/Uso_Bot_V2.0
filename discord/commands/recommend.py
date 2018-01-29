12a580964cbe17995d1c9af7adfc7f5552f8a34f
Renondedju <lefantomedelaredstone@gmail.com>
2018-01-29 17:53:34 +0100

    Created a basic recommend command for discord

--- discord/commands/recommend.py
+++ discord/commands/recommend.py
@@ -13,8 +13,9 @@ sys.path.append(os.path.realpath('../'))
 from libs.osuapi         import *
 from libs.user           import User
 from libs.beatmap        import Beatmap
-from libs                import pyttanko
+from libs.preset         import Preset
 from libs.recommendation import REngine
+from libs                import pyttanko
 
 class Recommendation:
     def __init__(self, bot):
@@ -22,15 +23,28 @@ class Recommendation:
         self.engine = REngine()
 
     @commands.command(pass_context=True, aliases=['r'])
-    async def recommend(self, ctx, inputs=None):
-        if inputs:
-            # TODO parse inputs for fine control
+    async def recommend(self, ctx, mode='default', power = 1.05):
+
+        user = User(discord_id = ctx.author.id)
+        if (user.is_empty()):
+            await ctx.send('User not linked')
+            return
+
+        preset = Preset(user, mode = mode, power = power)
+
+        if not preset.mode_exists(mode):
+            await ctx.send('This preset does not exist !')
+            return
+
+        self.engine.recommend(preset, 1)
 
         for i in range(len(self.engine.recommendatons)):
-            bmdb = self.engines.recommendatons[i]
-            mods = process_mods(self.engine.mods[i], bmdb)
-            bmap = pyttanko.parser().map(open(bmdb.beatmaps_path + mapid + '.osu'))
-            await self.map_embed(ctx, bmdb, bmap, pyttanko.mods_from_str(mods.lower()))
+            bmdb = self.engine.recommendatons[i]
+            mods = self.engine.mods[i]
+
+            url = 'https://osu.ppy.sh/beatmapsets/{}#osu/{}'.format(bmdb.beatmapset_id, bmdb.beatmap_id)
+
+            await ctx.send(url + " - " + mod_emoji(mods) + " using preset named '" + mode + "' power = " + str(power))
 
     async def map_embed(self, ctx, bmdb, bmap, mods):
         # Gets the stars and pp values for the map with the given mods
@@ -61,19 +75,20 @@ class Recommendation:
         await ctx.message.channel.send(embed=em)
 
 def mod_emoji(mods):
-    """Because emoji >= letters?"""
-    mods = mods.replace("EZ", "<:mod_easy:327800791631134731>")
+    """ Because emoji >= letters? """
     mods = mods.replace("SD", "<:mod_suddendeath:327800921113231361>")
-    mods = mods.replace("SO", "<:mod_spunout:327800910249984001>")
-    mods = mods.replace("HR", "<:mod_hardrock:327800817711054858>")
-    mods = mods.replace("PF", "<:mod_perfect:327800879019458571>")
-    mods = mods.replace("DT", "<:mod_doubletime:327800759741579265>")
-    mods = mods.replace("NC", "<:mod_nightcore:327800859989901312>")
-    mods = mods.replace("HD", "<:mod_hidden:328172007931904002>")
-    mods = mods.replace("FL", "<:mod_flashlight:327800804037885962>")
-    mods = mods.replace("RL", "<:mod_relax:327800900318134273>")
-    mods = mods.replace("AP", "<:mod_auto:327800780444794880>")
-    mods = mods.replace("NF", "<:mod_nofail:327800869523554315>")
+    mods = mods.replace("FL", "<:mod_flashlight:327800804037885962>" )
+    mods = mods.replace("DT", "<:mod_doubletime:327800759741579265>" )
+    mods = mods.replace("NC", "<:mod_nightcore:327800859989901312>"  )
+    mods = mods.replace("HR", "<:mod_hardrock:327800817711054858>"   )
+    mods = mods.replace("SO", "<:mod_spunout:327800910249984001>"    )
+    mods = mods.replace("PF", "<:mod_perfect:327800879019458571>"    )
+    mods = mods.replace("HD", "<:mod_hidden:328172007931904002>"     )
+    mods = mods.replace("NF", "<:mod_nofail:327800869523554315>"     )
+    mods = mods.replace("RL", "<:mod_relax:327800900318134273>"      )
+    mods = mods.replace("EZ", "<:mod_easy:327800791631134731>"       )
+    mods = mods.replace("AP", "<:mod_auto:327800780444794880>"       )
+
     return mods
 
 def process_mods(mods, bmap):
